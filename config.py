558fb35e0b76d98cb31c7f39a584a68242b9a245
Karl Apocalypse <info@reeknersprook.de>
2013-10-27 19:05:46 +0100

    TODO added

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 INFO = {
-    'version': '0.2.3',
+    'version': '0.3.0',
     'usage': 'use <ESC> to quit and <SPACE> to pause and continue',
 }
 
 
 SETTINGS = {
-    'clusters': 10,
+    'clusters': 20,
     'cluster_size': 7,
     'width': 1200,
     'height': 700,
--- tourplanner.py
+++ tourplanner.py
@@ -2,6 +2,8 @@ from client import ClientsCollection
 from tour import calculate_tours
 from config import SETTINGS, INFO
 
+# TODO: zur gezielten tour-area-vergrösserung Entfernung zu benachbarten clients checken
+# TODO: find_tour_clients sollte bei clients > cluster_size überzählige clients rauswerfen
 
 if __name__ == '__main__':
     all_clients = ClientsCollection(**SETTINGS)
