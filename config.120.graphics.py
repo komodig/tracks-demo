b8ec3ad6840026b66f066c579455ab5edcc57bb2
Magnus Fiedler <jack@rhea.(none)>
2014-03-08 03:40:37 +0100

    add some config files was the plan

---
+++ config.120.graphics.py
@@ -0,0 +1,42 @@
+INFO = {
+    'version': '0.9.2',
+    'usage': 'use <ESC> to quit and <SPACE> to pause and continue',
+}
+
+
+SETTINGS = {
+    'clients': 120,
+    'cluster_size_min': 5,
+    'cluster_size_max': 7,
+    'width': 1200,
+    'height': 700,
+}
+
+
+DIMENSION = [
+{
+    'x_factor': 0.2,
+    'y_factor': 0.2,
+},
+]
+
+DISPLAY = {
+    'intro': True,
+    'clients_intro': False,
+    'dimensions': True,
+    'dimensions_slow': True,
+    'unite_areas': True,
+    'routing': {
+        'all': False,
+        'best_starter': False,
+    },
+    'clients': {
+        'init': False,
+        'append':False,
+    },
+}
+
+TEST = {
+    'long_term': False,
+    'level': 1,
+}
---
+++ config.120.minimal.py
@@ -0,0 +1,42 @@
+INFO = {
+    'version': '0.9.2',
+    'usage': 'use <ESC> to quit and <SPACE> to pause and continue',
+}
+
+
+SETTINGS = {
+    'clients': 120,
+    'cluster_size_min': 5,
+    'cluster_size_max': 7,
+    'width': 1200,
+    'height': 700,
+}
+
+
+DIMENSION = [
+{
+    'x_factor': 0.2,
+    'y_factor': 0.2,
+},
+]
+
+DISPLAY = {
+    'intro': False,
+    'clients_intro': False,
+    'dimensions': False,
+    'dimensions_slow': False,
+    'unite_areas': False,
+    'routing': {
+        'all': False,
+        'best_starter': False,
+    },
+    'clients': {
+        'init': False,
+        'append':False,
+    },
+}
+
+TEST = {
+    'long_term': False,
+    'level': 1,
+}
